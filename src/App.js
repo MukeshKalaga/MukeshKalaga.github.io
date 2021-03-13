2635604637efe0ca452322d815df294322c25ced
Mukesh Kalaga <kalagamukesh@gmail.com>
2021-03-13 17:00:11 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import me from './assets/images/mukesh.jpg';
+import { MDBBtn, MDBCard, MDBCardBody } from 'mdbreact';
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="main-container">
+          <div className="text-center">
+            <img className="rounded-circle" height="200" src={me} />
+            <h2 className="title-name">Mukesh Kalaga</h2>
+            <p className="text-white text-uppercase" style={{fontSize:14}}> - Fullstack | Mern stack Developer</p>
+          </div>
+          <a className="hire-me-btn rounded-pill">Hire Me</a>
+        </div>
+        <div className="p-3 text-center">
+          <MDBCard className="" style={{marginTop:-40,maxWidth:700,display:"inline-block",width:"100%",height:300}}>
+            <MDBCardBody>
+              <h4 className="recent-work-title">Recent Work</h4>
+            </MDBCardBody>
+          </MDBCard>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
