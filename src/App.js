2cfc38265b292e3b9311ef2a62e12c6fc69a5ec9
Mukesh Kalaga <kalagamukesh@gmail.com>
2022-06-22 10:53:57 +0530

    init

--- src/App.js
+++ src/App.js
@@ -10,17 +10,17 @@ class App extends React.Component {
           <div className="text-center">
             <img className="rounded-circle" height="200" src={me} />
             <h2 className="title-name">Mukesh Kalaga</h2>
-            <p className="text-white text-uppercase" style={{fontSize:14}}> - Fullstack | Mern stack Developer</p>
+            <p className="text-white text-uppercase" style={{fontSize:14}}> - Fullstack Developer</p>
           </div>
-          <a className="hire-me-btn rounded-pill">Hire Me</a>
+          <a className="hire-me-btn rounded-pill" href="tel:7661959689">Hire Me</a>
         </div>
-        <div className="p-3 text-center">
+        {/* <div className="p-3 text-center">
           <MDBCard className="" style={{marginTop:-40,maxWidth:700,display:"inline-block",width:"100%",height:300}}>
             <MDBCardBody>
               <h4 className="recent-work-title">Recent Work</h4>
             </MDBCardBody>
           </MDBCard>
-        </div>
+        </div> */}
       </>
     );
   }
